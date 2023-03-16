38a2868dd5d735fdebb527e204e514c3e92f3c64
Chanella <c.chaaajong@unasat.sr>
2023-03-15 23:46:49 -0300

    Initial

--- src/main/webapp/js/reports.js
+++ src/main/webapp/js/reports.js
@@ -5,14 +5,14 @@ function getQuarterReport() {
         type: "GET",
         success: function(response) {
             response.sort(function(a, b) {
-                return new Date(a.borrow_date) - new Date(b.borrow_date);
+                return new Date(a.borrowDate) - new Date(b.borrowDate);
             });
             var reportData = document.getElementById('report-data');
             reportData.innerHTML = '';
             var ul = document.createElement('ul');
             response.forEach(function(item) {
                 var li = document.createElement('li');
-                li.innerHTML = 'ID: ' + item.id + ' Borrow Date: ' + item.borrow_date;
+                li.innerHTML = 'ID: ' + item.book.id + ', ISBN: ' + item.book.isbn + ', Title: ' + item.book.title + ', Subtitle: ' + item.book.subtitle + ', Borrow Date: ' + item.borrowDate;
                 ul.appendChild(li);
             });
             reportData.appendChild(ul);
@@ -23,21 +23,20 @@ function getQuarterReport() {
     });
 }
 
-// getYearReport function
 function getYearReport() {
     $.ajax({
         url: "http://localhost:8081/LibraryFrontWeb_war_exploded/api/reports/year",
         type: "GET",
         success: function(response) {
             response.sort(function(a, b) {
-                return new Date(a.borrow_date) - new Date(b.borrow_date);
+                return new Date(a.borrowDate) - new Date(b.borrowDate);
             });
             var reportData = document.getElementById('report-data');
             reportData.innerHTML = '';
             var ul = document.createElement('ul');
             response.forEach(function(item) {
                 var li = document.createElement('li');
-                li.innerHTML = 'ID: ' + item.id + ', Borrow Date: ' + item.borrow_date;
+                li.innerHTML = 'ID: ' + item.book.id + ', ISBN: ' + item.book.isbn + ', Title: ' + item.book.title + ', Subtitle: ' + item.book.subtitle + ', Borrow Date: ' + item.borrowDate;
                 ul.appendChild(li);
             });
             reportData.appendChild(ul);
@@ -50,8 +49,12 @@ function getYearReport() {
 
 $(document).ready(function() {
     // Attach a click event to the button with ID quarter-btn
-    $("#quarter-btn").click(getQuarterReport);
+    $("#quarter-btn").click(function(){
+        getQuarterReport();
+});
 
     // Attach a click event to the button with ID year-btn
-    $("#year-btn").click(getYearReport);
+    $("#year-btn").click(function(){
+        getYearReport();
+    });
 });
