1764949298c91b85edff6ae59d0d20806784c626
Chanella <c.chaaajong@unasat.sr>
2023-03-16 13:05:09 -0300

    Initial

--- src/main/webapp/js/membership.js
+++ src/main/webapp/js/membership.js
@@ -1,20 +1,22 @@
 function getMembership() {
-    var membershipType = $("#membershipType").val();
+    // Get the selected membership type
+    const membershipType = document.getElementById("membershipType").value;
 
+    // Make an AJAX request to retrieve the membership details
     $.ajax({
+        url: "http://localhost:8081/LibraryFrontWeb_war_exploded/api/memberships/getMembership",
         type: "POST",
-        url: "http://localhost:8080/Library/api/memberships/getMembership",
-        data: {
-            membershipType: membershipType
-        },
-        success: function (response) {
+        data: { type: membershipType },
+        success: function(response) {
+            // Update the fee and privilege elements with the returned data
             $("#fee").text(response.fee);
             $("#privilege").text(response.privilege);
         },
-        error: function () {
-            alert("An error occurred.");
+        error: function(error) {
+            console.log(error);
         }
     });
 }
+
 
 
