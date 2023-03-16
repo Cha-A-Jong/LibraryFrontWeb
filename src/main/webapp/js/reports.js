7166a7b03e0b84d64f74793ca0028c0660558ef1
Chanella <c.chaaajong@unasat.sr>
2023-03-16 10:45:01 -0300

    Initial

--- src/main/webapp/js/reports.js
+++ src/main/webapp/js/reports.js
@@ -1,60 +1,18 @@
-// getQuarterReport function
-function getQuarterReport() {
-    $.ajax({
-        url: "http://localhost:8081/LibraryFrontWeb_war_exploded/api/reports/quarter",
-        type: "GET",
-        success: function(response) {
-            response.sort(function(a, b) {
-                return new Date(a.borrowDate) - new Date(b.borrowDate);
-            });
-            var reportData = document.getElementById('report-data');
-            reportData.innerHTML = '';
-            var ul = document.createElement('ul');
-            response.forEach(function(item) {
-                var li = document.createElement('li');
-                li.innerHTML = 'ID: ' + item.book.id + ', ISBN: ' + item.book.isbn + ', Title: ' + item.book.title + ', Subtitle: ' + item.book.subtitle + ', Borrow Date: ' + item.borrowDate;
-                ul.appendChild(li);
-            });
-            reportData.appendChild(ul);
-        },
-        error: function(xhr, status, error) {
-            console.log("Error: " + error); // handle error
+function getReport() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const report = JSON.parse(xhr.responseText);
+            const reportDiv = document.getElementById("report");
+            let reportHTML = "<ul>";
+            for (let i = 0; i < report.length; i++) {
+                const book = report[i];
+                reportHTML += `<li>${book.title} (${book.isbn})</li>`;
+            }
+            reportHTML += "</ul>";
+            reportDiv.innerHTML = reportHTML;
         }
-    });
+    };
+    xhr.open("GET", "http://localhost:8081/LibraryFrontWeb_war_exploded/api/reports/quarter");
+    xhr.send();
 }
-
-function getYearReport() {
-    $.ajax({
-        url: "http://localhost:8081/LibraryFrontWeb_war_exploded/api/reports/year",
-        type: "GET",
-        success: function(response) {
-            response.sort(function(a, b) {
-                return new Date(a.borrowDate) - new Date(b.borrowDate);
-            });
-            var reportData = document.getElementById('report-data');
-            reportData.innerHTML = '';
-            var ul = document.createElement('ul');
-            response.forEach(function(item) {
-                var li = document.createElement('li');
-                li.innerHTML = 'ID: ' + item.book.id + ', ISBN: ' + item.book.isbn + ', Title: ' + item.book.title + ', Subtitle: ' + item.book.subtitle + ', Borrow Date: ' + item.borrowDate;
-                ul.appendChild(li);
-            });
-            reportData.appendChild(ul);
-        },
-        error: function(xhr, status, error) {
-            console.log("Error: " + error); // handle error
-        }
-    });
-}
-
-$(document).ready(function() {
-    // Attach a click event to the button with ID quarter-btn
-    $("#quarter-btn").click(function(){
-        getQuarterReport();
-});
-
-    // Attach a click event to the button with ID year-btn
-    $("#year-btn").click(function(){
-        getYearReport();
-    });
-});
